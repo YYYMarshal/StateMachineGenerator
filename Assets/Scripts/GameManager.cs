3d8d40c02f38fd677c10837cbe1f2e2f6cb4c686
YYYMarshal <38450127+YYYMarshal@users.noreply.github.com>
2020-08-07 23:49:32 +0800

    添加项目文件。

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject BGGameManager;
+    private GameObject btnCreateState;
+    private void Awake()
+    {
+        BGGameManager = GameObject.Find("BGGameManager");
+        btnCreateState = GameObject.Find("BtnCreateState");
+        BGGameManager.GetComponent<Button>().onClick.AddListener(BGGameManagerOnClick);
+        btnCreateState.GetComponent<Button>().onClick.AddListener(BtnCreateStateOnClick);
+        //Button btn = null;
+        
+        btnCreateState.SetActive(false);
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            btnCreateState.SetActive(true);
+            btnCreateState.transform.localPosition = GetMousePosition2D(true);
+        }
+    }
+
+    private Vector2 GetMousePosition2D(bool isReduce)
+    {
+        float X = 0f;
+        float Y = 0f;
+        if (isReduce)
+        {
+            X = Input.mousePosition.x - Screen.width / 2f;
+            Y = Input.mousePosition.y - Screen.height / 2f;
+        }
+        else
+        {
+            X = Input.mousePosition.x;
+            Y = Input.mousePosition.y;
+        }
+        return new Vector2(X, Y);
+    }
+    private void BGGameManagerOnClick()
+    {
+        if (btnCreateState.activeSelf)
+            btnCreateState.SetActive(false);
+    }
+    private void BtnCreateStateOnClick()
+    {
+        //UnityEditor.AssetDatabase.LoadAssetAtPath
+        btnCreateState.SetActive(false);
+        GameObject itemState = Instantiate(Resources.Load<GameObject>("Prefabs/ItemState"),
+            GetMousePosition2D(false), this.transform.rotation);
+        itemState.transform.SetParent(BGGameManager.transform);
+        itemState.AddComponent<ItemStateOnDrag>();
+    }
+}
---
+++ Assets/Scripts/ItemStateOnDrag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemStateOnDrag : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        SetDraggedPosition(eventData);
+    }
+
+    //https://www.cnblogs.com/ylwshzh/p/4460915.html
+    private void SetDraggedPosition(PointerEventData eventData)
+    {
+        RectTransform rectTrans = gameObject.GetComponent<RectTransform>();
+        // transform the screen point to world point int rectangle
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            rectTrans, eventData.position, eventData.pressEventCamera, out Vector3 globalMousePos))
+        {
+            rectTrans.position = globalMousePos;
+        }
+    }
+}
